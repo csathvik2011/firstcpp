79f021186e99cd107dcf67871f00319b4ee0a8b8
csathvik2011 <97310465+csathvik2011@users.noreply.github.com>
2022-04-12 17:57:56 -0400

    Update first.cpp

--- first.cpp
+++ first.cpp
@@ -7,6 +7,19 @@ using namespace std;
 int main() {
     cout << "Hello this is my first cpp" <<endl;
     cout << "Video 24:50\n";
-    x = 2022;
+    int year = 2022;
+    double price = 9999.99
+    char status = 'N';
+    string make = "Civic"
+    string model = "Honda";
+    bool for_sale = true;
+    for_sale = false;
+    string car = make+" "+ model
+    price = price*0.9;
+    cout << "Year" << year << endl;
+    cout << "Price: " << price << endl;
+    cout << "Status: " << status << endl;
+    cout << "New: " << model << endl;
+    cout << "Availability: " << for_sale << endl;
     return 0;
 }
