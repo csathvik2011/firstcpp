dfaf1a36ecc533a1a23b79b790d9103b0a8f4cb8
csathvik2011 <97310465+csathvik2011@users.noreply.github.com>
2022-04-12 16:08:49 -0400

    Update first.cpp

--- first.cpp
+++ first.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
 using namespace std;
-
+//This is a comment
+/*Same as this
+*/
 int main() {
     cout << "Hello this is my first cpp" <<endl;
-    cout << "Video 17:50\n";
+    cout << "Video 24:50\n";
+    x = 2022;
     return 0;
 }
